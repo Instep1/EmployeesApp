7bae7757b483b6a8a6162d6e338694e4a436ecc3
Maksim <lvutinwork@gmail.com>
2025-04-10 17:29:46 +0300

    feat: added function to calculate in component app_info / added function to increase salary and rise employees

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,12 @@
 import './app-info.css';
 
-const AppInfo = () => {
+const AppInfo = (props) => {
+    const { employees, employeesOnIncrease } = props;
     return (
         <div className="app-info">
             <h1>Учёт сотрудников в компании Nicrosoft</h1>
-            <h2>Общее число сотрудников: </h2>
-            <h2>Премию получат: </h2>
+            <h2>Общее число сотрудников: {employees}</h2>
+            <h2>Премию получат: {employeesOnIncrease}</h2>
         </div>
     )
 }
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,6 @@
+import { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,28 +9,85 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data : [
+                {name: 'Maksim L.', salary: 800, increase: true, star: true, id: uuidv4()},
+                {name: 'Andrey K.', salary: 3000, increase: false, star: false, id: uuidv4()},
+                {name: 'Michael A.', salary: 5000, increase: true, star: false, id: uuidv4()},
+                {name: 'Anar S.', salary: 10000, increase: false, star: false, id: uuidv4()},
+            ]
+        }
+    }
 
-    const data = [
-        {name: 'Maksim L.', salary: 800, increase: true, id: 1},
-        {name: 'Andrey K.', salary: 3000, increase: false, id: 2},
-        {name: 'Michael A.', salary: 5000, increase: true, id: 3},
-        {name: 'Anar S.', salary: 10000, increase: false, id: 4},
-    ];
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
 
-    return (
-        <div className="app">
-            <AppInfo/>
+    addItem = (name, salary) => {
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            star: false,
+            id: uuidv4()
+        }
+        if (name.length < 3 || !salary) {
+            return;
+        } else {
+            this.setState(({data}) => {
+            
+                return {
+                    data: [...data, {...newItem}]
+                }
+            });
+        }
+        
+    }
 
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+    onToggleProp = (id, prop) => {
+
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item;
+            })
+        }))
+    }
+
+    render() {
+        const { data } = this.state;
+        const employees = data.length;
+        const employeesOnIncrease = data.filter(item => item.increase === true).length;
+        return (
+            <div className="app">
+                <AppInfo
+                    employees={employees}
+                    employeesOnIncrease={employeesOnIncrease}
+                    />
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+    
+                <EmployeesList 
+                    data={this.state.data}
+                    onDelete={this.deleteItem}
+                    onToggleProp={this.onToggleProp}/>
+                <EmployeesAddForm
+                    onAdd={this.addItem}/>
             </div>
-
-            <EmployeesList data={data}/>
-            <EmployeesAddForm/>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
